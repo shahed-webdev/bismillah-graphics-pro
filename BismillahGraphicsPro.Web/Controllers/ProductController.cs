cd4edbcf647c7319d6239e9edb1191f83c60674b
MUHIBOLLAH <muhebollah.diu@gmail.com>
2022-01-16 12:05:00 +0600

    mesurement unit page added

---
+++ BismillahGraphicsPro.Web/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BismillahGraphicsPro.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult MeasurementUnit()
+        {
+            return View();
+        }
+    }
+}
