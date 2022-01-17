4e8beb7f24cbef63bfd7de16f8188bf896b4c4e2
MUHIBOLLAH <muhebollah.diu@gmail.com>
2022-01-17 20:23:20 +0600

    bug fix

--- BismillahGraphicsPro.Web/Controllers/AdminController.cs
+++ BismillahGraphicsPro.Web/Controllers/AdminController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BismillahGraphicsPro.Web.Controllers
 {
-    // [Authorize(Roles = "Admin")]
+    [Authorize(Roles = "Admin,SubAdmin")]
     public class AdminController : Controller
     {
         public IActionResult Index()
