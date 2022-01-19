4b5be21ebecd632020644fd7d605423b578f30ba
MUHIBOLLAH <muhebollah.diu@gmail.com>
2022-01-19 15:51:23 +0600

    view component added

---
+++ BismillahGraphicsPro.Web/ViewComponents/BranchInfoViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using BismillahGraphicsPro.BusinessLogic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BismillahGraphicsPro.Web.ViewComponents
+{
+    [Authorize]
+    public class BranchInfoViewComponent : ViewComponent
+    {
+        private readonly IRegistrationCore _registration;
+
+        public BranchInfoViewComponent(IRegistrationCore registration)
+        {
+            _registration = registration;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string filter)
+        {
+            var model = _registration.UserTypeByUserName(User.Identity.Name);
+            return View("Default", model);
+        }
+    }
+}
