850ca192b1590d9ba7d4f9a8a777466b44dc1d42
MUHIBOLLAH <muhebollah.diu@gmail.com>
2022-02-02 17:23:16 +0600

    profile update done

---
+++ BismillahGraphicsPro.Web/ViewComponents/ProfileViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using BismillahGraphicsPro.BusinessLogic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BismillahGraphicsPro.Web.ViewComponents
+{
+    [Authorize]
+    public class ProfileViewComponent : ViewComponent
+    {
+        private readonly IRegistrationCore _registration;
+
+        public ProfileViewComponent(IRegistrationCore registration)
+        {
+            _registration = registration;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = await _registration.GetUserAsync(User.Identity.Name);
+            return View("Default", model.Data);
+        }
+    }
+}
